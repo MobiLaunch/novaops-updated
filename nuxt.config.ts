da73f1bd919bb49b8fcd7946e2916a2a635e0c42
MobiLaunch <149342683+MobiLaunch@users.noreply.github.com>
2026-02-20 10:11:28 -0600

    removed sw.js, added my own to clean cache after refresh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@vite-pwa/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
 
   runtimeConfig: {
     public: {
@@ -17,58 +17,6 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   ssr: false,
-
-  pwa: {
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'NovaOps',
-      short_name: 'NovaOps',
-      description: 'Repair Shop Management',
-      theme_color: '#1e1e2e',
-      background_color: '#1e1e2e',
-      display: 'standalone',
-      orientation: 'portrait',
-      scope: '/',
-      start_url: '/',
-      icons: [
-        {
-          src: '/icon-192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        },
-      ],
-    },
-    workbox: {
-      navigateFallback: '/index.html',
-      navigateFallbackDenylist: [/^\/api\//],
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-      cleanupOutdatedCaches: true,
-      skipWaiting: true,
-      clientsClaim: true,
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/pkluxhfcdbdjsyypxvrg\.supabase\.co\/.*/i,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'supabase-cache',
-            expiration: { maxEntries: 50, maxAgeSeconds: 300 },
-          },
-        },
-      ],
-    },
-    client: {
-      installPrompt: true,
-    },
-    devOptions: {
-      enabled: false,
-    },
-  },
 
   postcss: {
     plugins: {
---
+++ public/sw.js
@@ -0,0 +1,9 @@
+// public/sw.js
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  )
+})
